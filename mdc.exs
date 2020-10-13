3141703eeeaec63e4e587a108d786abf01b0c43b
Adriano Croco <adriano.daniel@pravaler.com.br>
2020-10-13 19:49:17 -0300

    fix mdc

--- mdc.exs
+++ mdc.exs
@@ -1,24 +1,19 @@
 defmodule Calculator do
   def mcd(a, b) do
-     #IO.puts("mcd: #{a},#{b}")
-    calculate(a, b, 2, []) |> IO.inspect()
-  end
-
-  def calculate(a, b, x, _acc) when x < a and x < b do
-    IO.puts("mcd: #{a},#{b}")
+    calculate(a, b, 2, 1)
   end
 
   def calculate(a, b, x, acc) do
-    IO.puts(x)
     if rem(a, x) == 0 and rem(b, x) == 0 do
-      calculate(div(a, x), div(b, x), x, acc ++ x)
+      calculate(div(a, x), div(b, x), x, acc * x)
     else
-      next = x+1
-      if next < a and next < b do
-        calculate(a, b, next, acc)
+      if x < a and x < b do
+        calculate(a, b, x + 1, acc)
+      else
+        IO.puts(acc)
       end
     end
   end
 end
 
-Calculator.mcd(15,45)
+Calculator.mcd(15, 45)
